4aeb1362fcb470dbf976c171fd5853ef45980434
Sobolev Anatolii <sobolev.anatoly@gmail.com>
2019-08-27 21:08:32 +0300

    scratch

---
+++ resizer.php
@@ -0,0 +1,11 @@
+<?php
+/*
+  Plugin Name: Resizer
+  Plugin URI: https://github.com/SobolevAnatoly/resizer
+  Description: Resize images by link
+  Version: 0.0.1 Beta
+  Author: Barbarian
+  Author URI:
+  License: GPLv2
+ */
+
